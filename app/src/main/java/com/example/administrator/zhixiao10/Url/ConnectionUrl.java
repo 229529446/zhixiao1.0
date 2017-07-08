0e14c5189213708272f9bcfe37c69a4807a54ae5
jerry <229529446@qq.com>
2017-07-08 14:08:23 +0800

    知校项目android

--- app/src/main/java/com/example/administrator/zhixiao10/Url/ConnectionUrl.java
+++ app/src/main/java/com/example/administrator/zhixiao10/Url/ConnectionUrl.java
@@ -4,8 +4,8 @@ package com.example.administrator.zhixiao10.Url;
  * Created by Administrator on 2016/12/4 0004.
  */
 public class ConnectionUrl {
-    public static final String SERVICE_URL = "http://sese666.com.cn:8080/zhixiaoService/servlet/";
-    public static final String ROOT_URL = "http://sese666.com.cn:8080/zhixiaoService/";
+    public static final String SERVICE_URL = "http://119.29.38.74:8080/zhixiaoService/servlet/";
+    public static final String ROOT_URL = "http://119.29.38.74:8080/zhixiaoService/";
 
-    public static final String CHAT_SERVICE_URL = "sese666.com.cn";
+    public static final String CHAT_SERVICE_URL = "119.29.38.74";
 }
